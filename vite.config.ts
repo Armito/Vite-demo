cf07e41190366693dc8c976b3ea6caceb6feda26
曾星 <zengxing@udesk.cn>
2022-02-09 15:09:55 +0800

    fix: eslint bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-import Pages from "vite-plugin-pages";
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueSetupExtend(),
-    Pages({
-      exclude: ['**/components/*.vue']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '~': path.resolve('./'),
+    plugins: [
+        vue(),
+        VueSetupExtend(),
+        Pages({
+            exclude: ['**/components/*.vue']
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '~': path.resolve('./')
+        }
     }
-  },
 })

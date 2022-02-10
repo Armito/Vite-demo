1bf3aee86b1bb09718f4effe9a4ed4bc28fcc222
Armito <1656318310@qq.com>
2022-02-10 20:59:18 +0800

    feat: add some func

--- src/components/Count/index.vue
+++ src/components/Count/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: Armito
+ * @Date: 2022-02-10 19:51:46
+ * @LastEditTime: 2022-02-10 20:57:13
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \Vite-demo\src\components\Count\index.vue
+-->
 <script setup lang="ts">
 import { useCount } from './useCount'
 
@@ -10,7 +18,7 @@ withDefaults(
     }
 )
 
-const { count, increment } = useCount()
+const { count, increment, decrement, double } = useCount()
 
 defineExpose({
     increment
@@ -18,9 +26,17 @@ defineExpose({
 </script>
 
 <template>
-    <button v-marker type="button" @click="increment">
-        {{ msg }} count is: {{ count }}
-    </button>
+    <el-card>
+        <template #header>
+            <div v-marker>加减乘除</div>
+        </template>
+        <template #default>
+            <div>{{ msg }} count is: {{ count }}</div>
+        </template>
+    </el-card>
+    <el-button type="button" @click="increment">+++</el-button>
+    <el-button type="button" @click="decrement">---</el-button>
+    <el-button type="button" @click="double">x2</el-button>
 </template>
 
 <style scoped></style>
--- src/components/Count/useCount.ts
+++ src/components/Count/useCount.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Armito
+ * @Date: 2022-02-10 19:51:46
+ * @LastEditTime: 2022-02-10 20:45:02
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \Vite-demo\src\components\Count\useCount.ts
+ */
 import { ref } from 'vue'
 
 export const useCount = () => {
@@ -7,8 +15,18 @@ export const useCount = () => {
         count.value++
     }
 
+    const decrement = () => {
+        count.value--
+    }
+
+    const double = () => {
+        count.value = count.value * 2
+    }
+
     return {
         count,
-        increment
+        increment,
+        decrement,
+        double
     }
 }

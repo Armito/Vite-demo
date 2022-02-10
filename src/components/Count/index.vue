d018edf553504c9c24eea24da76bbd4a08947492
Armito <1656318310@qq.com>
2022-02-10 21:08:52 +0800

    fix: el-button type bug

--- src/components/Count/index.vue
+++ src/components/Count/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Armito
  * @Date: 2022-02-10 19:51:46
- * @LastEditTime: 2022-02-10 20:57:13
+ * @LastEditTime: 2022-02-10 21:02:03
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vite-demo\src\components\Count\index.vue
@@ -34,9 +34,9 @@ defineExpose({
             <div>{{ msg }} count is: {{ count }}</div>
         </template>
     </el-card>
-    <el-button type="button" @click="increment">+++</el-button>
-    <el-button type="button" @click="decrement">---</el-button>
-    <el-button type="button" @click="double">x2</el-button>
+    <el-button type="success" @click="increment">+++</el-button>
+    <el-button type="danger" @click="decrement">---</el-button>
+    <el-button type="warning" @click="double">x2</el-button>
 </template>
 
 <style scoped></style>

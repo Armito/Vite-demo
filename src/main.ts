8d3e2dbabab1fd1805e9f66c1318cb61b0dbcf1f
Armito <1656318310@qq.com>
2022-05-02 15:02:41 +0800

    fix: rename

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Armito
  * @Date: 2022-02-10 19:51:46
- * @LastEditTime: 2022-03-17 21:38:16
+ * @LastEditTime: 2022-04-09 10:54:17
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vite-demo\src\main.ts
@@ -12,7 +12,6 @@ import router from './router'
 import { createPinia } from 'pinia'
 import registerDirectives from './directives'
 import elementPlus from 'element-plus'
-import ArComponent from './common/ArComponent'
 import './styles/global.less'
 import 'element-plus/theme-chalk/index.css'
 
@@ -21,5 +20,4 @@ createApp(App)
     .use(createPinia())
     .use(registerDirectives)
     .use(elementPlus)
-    .use(ArComponent)
     .mount('#app')
